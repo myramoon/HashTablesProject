b5d4a645dce3c828e5dcd1a3ca0b2d0420700947
anamfazal <anamfazal94@gmail.com>
2020-10-29 01:21:12 +0530

    Added: functionality code and test cases to find frequency of words in a large phrase

--- src/test/java/com/hashtableoperations/MyHashTableTest.java
+++ src/test/java/com/hashtableoperations/MyHashTableTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class MyHashTableTest {
 
     @Test
-    public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
+    public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordToFrequency() {
         String sentence = "To be or not to be";
         MyHashTable<String , Integer> myHashTable = new MyHashTable<>();
         String[] words = sentence.toLowerCase().split(" ");
@@ -19,5 +19,22 @@ public class MyHashTableTest {
         int frequency = myHashTable.get("to");
         System.out.println(myHashTable);
         Assert.assertEquals(2 , frequency);
+    }
+
+    @Test
+    public void givenALargeSentence_WhenWordsAreAddedToList_ShouldReturnWordParanoidFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+        MyLinkedHashMap<String , Integer> myLinkedHashMap = new MyLinkedHashMap();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null) value = 1;
+            else value = value + 1;
+            myLinkedHashMap.add(word , value);
+        }
+        int frequency = myLinkedHashMap.get("paranoid");
+        System.out.println(myLinkedHashMap);
+        Assert.assertEquals(3 , frequency);
     }
 }
